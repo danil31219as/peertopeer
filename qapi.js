2cbec8e5fc6516630cd5fb519e4af1932d6a79ac
danil31219as <57491208+danil31219as@users.noreply.github.com>
2021-06-26 22:15:52 +0400

    Changes

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60d76e9da9f62c0022ae3044",
+				"60d76e9da9f62c0022ae3046"
+			]
+		},
+		"60d76e9da9f62c0022ae3044": {
+			"id": "60d76e9da9f62c0022ae3044",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60d76e9da9f62c0022ae3046": {
+			"id": "60d76e9da9f62c0022ae3046",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
